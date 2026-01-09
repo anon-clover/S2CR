66c86ffead9a090aadfa94301cd50d3c4e8cf127
anon-clover <shaohuihui0312@gmail.com>
2026-01-09 17:59:01 +0800

    Update Templates.py

--- Templates.py
+++ Templates.py
@@ -1,8 +1,8 @@
 Prompt1="Generate accurate and concise answers to the question using the provided information."
 
 Prompt2="Extract entity–relation triples related to the input from the provided information.
-		\textit{Information}: Boutheina Jabnoun Marai is a Tunisian journalist and magazine publisher. She is …
-		\textit{Input}: What is Boutheina Jabnoun's occupation?\newline
-		\textit{Triples}: [(Boutheina Jabnoun Marai, occupation, Tunisian journalist), …]"
+		Information: Boutheina Jabnoun Marai is a Tunisian journalist and magazine publisher. She is …
+		Input: What is Boutheina Jabnoun's occupation?\newline
+		Triples: [(Boutheina Jabnoun Marai, occupation, Tunisian journalist), …]"
 
 
